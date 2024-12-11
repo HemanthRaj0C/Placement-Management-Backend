8e89c5f3ced6c38dc58f9c2185ec93137cc8ad10
HemanthRaj0C <personalhemanthraj@gmail.com>
2024-12-11 10:00:24 +0530

    [Add] Docker Files X_X

--- server.js
+++ server.js
@@ -30,9 +30,20 @@ app.use('/api', recruiterProfile);
 app.use('/api', interview);
 app.use('/api', quiz);
 
-mongoose.connect('mongodb://localhost:27017/placement-management', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>{console.log('Connected to MongoDB')})
-    .catch((err)=>{console.log('Error connecting to MongoDB', err)});
+mongoose.connect('mongodb://mongo:27017/placement-management', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  })
+  .then(() => { 
+    console.log('Connected to MongoDB');
+    console.log('Connection URI:', 'mongodb://mongo:27017/placement-management');
+  })
+  .catch((err) => { 
+    console.error('Error connecting to MongoDB', err);
+    console.error('Full error details:', JSON.stringify(err, null, 2));
+    console.error('Connection URI used:', 'mongodb://mongo:27017/placement-management');
+    process.exit(1); // Exit the process if DB connection fails
+  });
 
 const PORT = process.env.SERVER_PORT
 
